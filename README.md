131f541550a407e35687d9bb804c0cfb9a8233ff
Arpad Illyes <arpadillyes93@gmail.com>
2019-04-16 11:07:32 +0200

    index.js ready, add README and some test files

---
+++ README.md
@@ -0,0 +1,37 @@
+# Coding assignment
+
+My final solution is written in JavaScript and can be found at the `index.js` file. Some test files in JSON format are also included.
+
+### Usage
+
+The simplest way to run the code is by using the _Node.js_ environment. Open up a terminal at the root of the folder and type `node index`. The output will appear on the console.
+
+All the necessary funcionality is included in the `main()` function. Its parameters - the _input file_ and the _merge distance_ - can be changed at the top of the `index.js` file.
+
+Test files in other format (e.g. CSV) need to be parsed to an _array of objects_ at the top of the `main()` function, where each object must have an `arrivalTime`, `start`, `end` and an `action` field.
+
+##### Note
+
+To keep the code simple I omitted a validation step to check whether for all intervals in the input file `start < end` stands. It was assumed during testing.
+
+### How it works
+
+The `main()` function first creates an empty `state` _array_ and defines functions to modify it. The inner functions `addToState()`, `removeFromState()` and `deleteFromState()` all take one _interval object_ as their input and (possibly) alter the `state` as a _side effect_.
+
+The output is computed by the `mergeState()` function given a _merge distance_. This function runs in every step regardless whether the `state` was changed.
+
+### Deleting an interval
+
+When an interval `[a, b]` is signed for deletion the `deleteFromState()` first finds all the intervals in `state` that contain `[a, b]`. These intervals are then removed from `state` and replaced by the interval differences that remain after deletion.
+
+##### Caveats
+
+- Only _subsets_ of the intervals in the current `state` can be deleted. If an interval-to-be-deleted intersects with another one in the `state`, but is not fully contained in it, no deletion will take place
+- If parts of an interval is deleted later the _original_ interval cannot be removed
+- Upon deletion the remaining parts of an interval will appear as distinct elements in the `state` however the merge distance can be such that the action won't have any visible effects in the output
+
+### On efficiency
+
+This method computes the output in every step. Although I chose this approach since for smaller input sizes it was the easiest to implement it can get slow as the `state` grows.
+
+A better way would be to store the output separately and only _update_ the relevant parts of it when adding, removing or deleting an interval.
--- index.js
+++ index.js
@@ -1,31 +1,85 @@
 /* 
   Declare the main function parameters here
-
-  f: input file
-  d: merge distance
+    f: input file
+    d: merge distance
 */
 
-const f = require('./testFile');
-const d = 7;
-
-// TODO: Rename this to something better
-class StateElement {
-  constructor(start, end, subintervals) {
-    (this.start = start), (this.end = end), (this.subintervals = subintervals);
-  }
-}
+const f = require('./test1');
+const d = 3;
 
 const main = (file, distance) => {
-  const intervals = parseFile(file);
+  const { intervals } = file; // Parse input file here
   let state = [];
 
+  // --- Function definitions ---
+  const addToState = interval => {
+    const { start, end } = interval;
+    if (state.length === 0) {
+      state.push({ start, end });
+    } else {
+      let i = 0;
+      while (i < state.length && start > state[i].start) {
+        i++;
+      }
+      if (i < state.length && start === state[i].start) {
+        while (i < state.length && end > state[i].end) {
+          i++;
+        }
+      }
+      state.splice(i, 0, { start, end });
+    }
+  };
+
+  const removeFromState = interval => {
+    const { start, end } = interval;
+    const removeIndex = state.findIndex(
+      el => el.start === start && el.end === end
+    );
+    if (removeIndex > -1) {
+      state.splice(removeIndex, 1);
+    }
+  };
+
+  const deleteFromState = interval => {
+    const { start, end } = interval;
+    const newIntervals = state.filter(el => start >= el.start && end <= el.end);
+
+    newIntervals.forEach(el => {
+      removeFromState(el);
+      const leftSide = { start: el.start, end: start };
+      leftSide.start !== leftSide.end && addToState(leftSide);
+      const rightSide = { start: end, end: el.end };
+      rightSide.start !== rightSide.end && addToState(rightSide);
+    });
+  };
+
+  const mergeState = d => {
+    let output = [];
+    let i = 0;
+    while (i < state.length) {
+      const { start } = state[i];
+      let { end } = state[i];
+      while (i < state.length - 1 && state[i + 1].start <= end + d) {
+        end = Math.max(end, state[i + 1].end);
+        i++;
+      }
+      output.push(`[${[start, end]}]`);
+      i++;
+    }
+    return output;
+  };
+
+  // --- Process the input file ---
   for (let interval of intervals) {
     switch (interval.action) {
       case 'ADDED':
-        state = addInterval(interval, state);
+        addToState(interval);
         break;
       case 'REMOVED':
-        state = removeInterval(interval, state);
+        removeFromState(interval);
+        break;
+      case 'DELETED':
+        deleteFromState(interval);
         break;
       default:
         throw new Error(
@@ -33,129 +87,14 @@ const main = (file, distance) => {
         );
     }
 
-    const output = state.map(item => `[${[item.start, item.end]}]`);
+    const output = mergeState(distance);
     console.log(`
-      -------------------------------------------------------
-
-      ${interval.action}: [${[interval.start, interval.end]}]
-
-      ${output}`);
+      ${interval.action}: [${[
+      interval.start,
+      interval.end
+    ]}] --> OUTPUT: ${output}
+      `);
   }
 };
 
-// Parse the input file
-const parseFile = file => {
-  return file.intervals;
-};
-
-// Add interval and to state and merge it
-const addInterval = (target, state) => {
-  if (state.length > 0) {
-    const nextState = insertToState(target, state);
-    return nextState;
-  }
-
-  const { start, end } = target;
-  const firstInterval = new StateElement(start, end, [[start, end]]);
-  return [firstInterval];
-};
-
-// Get the position of the target interval to be inserted into state
-const getPosition = (target, state) => {
-  const { start, end } = target;
-  const pos = {};
-  let i = state.length;
-
-  // Find the position of the end
-  while (i > 0 && end <= state[i - 1].end) {
-    i--;
-  }
-  pos.endIndex = i;
-  pos.endIsIn =
-    i === state.length ? false : end >= state[i].start ? true : false;
-
-  // Find the position of the start
-  while (i > 0 && start <= state[i - 1].end) {
-    i--;
-  }
-  pos.startIndex = i;
-  pos.startIsIn =
-    i === state.length ? false : start >= state[i].start ? true : false;
-
-  return pos;
-};
-
-// Add target interval to state at the correct position and modify them accordingly (no merging)
-// FIXME: Don't modify state
-const insertToState = (target, state) => {
-  const pos = getPosition(target, state);
-  const { startIndex, startIsIn, endIndex, endIsIn } = pos;
-  const { start, end } = target;
-  let nextState;
-
-  if (startIndex === endIndex && startIsIn === endIsIn) {
-    if (startIsIn) {
-      nextState = [...state];
-      nextState[startIndex].subintervals = [
-        ...nextState[startIndex].subintervals,
-        [start, end]
-      ];
-    } else {
-      const front = state.slice(0, startIndex);
-      const back = state.slice(startIndex);
-      const newInterval = new StateElement(start, end, [[start, end]]);
-      nextState = [...front, newInterval, ...back];
-    }
-  } else {
-    const front = state.slice(0, startIndex);
-    const back = state.slice(endIndex + 1);
-
-    // Merge subintervals
-    let newSubintervals = [];
-    for (let i = startIndex; i <= Math.min(state.length - 1, endIndex); i++) {
-      newSubintervals = newSubintervals.concat(state[i].subintervals);
-    }
-    newSubintervals = newSubintervals.concat([[start, end]]);
-
-    const newInterval = {
-      start: startIsIn ? state[startIndex].start : start,
-      end: endIsIn ? state[endIndex].end : end,
-      subintervals: newSubintervals
-    };
-    nextState = [...front, newInterval, ...back];
-  }
-  return nextState;
-};
-
-// FIXME: Don't modify state and clean this up
-const removeInterval = (target, state) => {
-  let nextState = [...state];
-  const { start, end } = target;
-  const containsTargetInterval = subinterval =>
-    subinterval[0] === start && subinterval[1] === end;
-
-  const intervalIndex = state.findIndex(interval =>
-    interval.subintervals.some(containsTargetInterval)
-  );
-
-  if (intervalIndex > -1) {
-    const subintervals = nextState[intervalIndex].subintervals.filter(
-      subinterval => !(subinterval[0] === start && subinterval[1] === end)
-    );
-
-    if (subintervals.length === 0) {
-      return nextState.filter((interval, i) => i !== intervalIndex);
-    }
-
-    return nextState;
-  } else {
-    throw new Error(
-      `The interval [${[
-        start,
-        end
-      ]}] is either already removed or has never been added at the first place`
-    );
-  }
-};
-
-// main(f, d);
+main(f, d);
